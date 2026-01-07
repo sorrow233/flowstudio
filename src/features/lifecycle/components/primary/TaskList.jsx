cf319f22b8bfa302eb6123e1fafde4294d882e85
sorrow233 <sorrow233@proton.me>
2026-01-07 23:25:57 +0900

    修复补漏：增加 TaskList 滚动区域最小高度至 calc(100%+200px)，彻底解决 Stage 1 无法滚动的 Bug

--- src/features/lifecycle/components/primary/TaskList.jsx
+++ src/features/lifecycle/components/primary/TaskList.jsx
@@ -83,7 +83,7 @@ const TaskList = ({ tasks, projectId, activeStage, onToggle, onDelete, onAddTask
                 className="flex-1 overflow-y-auto px-8 pb-4 custom-scrollbar"
                 onScroll={onScroll}
             >
-                <div className="min-h-[101%] flex flex-col">
+                <div className="min-h-[calc(100%+200px)] flex flex-col">
                     <AnimatePresence mode="popLayout">
                         {localTasks.length > 0 ? (
                             <Reorder.Group
