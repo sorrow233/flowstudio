0ff5d587a69a09d17c42fdcaae6ac0b66f8794dc
sorrow233 <sorrow233@proton.me>
2026-01-07 17:44:09 +0900

    chore(root): RESET PROJECT TO V3.0.0
    - Deleted src/ and public/ directories
    - Created placeholder index.html for build pipeline
    - Bumped version to 3.0.0 (Clean Slate)

--- src/components/LoadingFallback.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingFallback = () => (
-    <div className="loading-container">
-        <div className="loading-spinner"></div>
-    </div>
-);
-
-export default LoadingFallback;
